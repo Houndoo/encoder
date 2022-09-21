006b8baab249ebae3940997b36a2067ebd95648b
Houndoo <99274855+Houndoo@users.noreply.github.com>
2022-09-20 20:13:42 -0400

    Initial Code

--- src/main/java/com/example/encoder/EncoderApplication.java
+++ src/main/java/com/example/encoder/EncoderApplication.java
@@ -1,13 +1,29 @@
 package com.example.encoder;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EncoderApplication {
+public class EncoderApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncoderApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		List<String> lines = Files.readAllLines(Paths.get("src/main/resources/input.csv"));
+		String base64Encoded = Base64.getEncoder().encodeToString(lines.get(0).getBytes());
+		byte[] test = Base64.getDecoder().decode(base64Encoded);
+		System.out.println("Hello World" + lines + base64Encoded + new String(test));
+		
 	}
 
 }
